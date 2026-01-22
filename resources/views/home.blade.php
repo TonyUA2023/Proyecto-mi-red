7577e9da16d73dda57fd264d447f9abb6f3bacbc
Antonnyy <167787920+Antonnyy@users.noreply.github.com>
2026-01-21 23:52:27 -0500

    Initial commit - Laravel project setup

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Mi Red Web</title>
+</head>
+<body>
+    <h1>Bienvenido a Mi Red Web</h1>
+    <p>Laravel está funcionando correctamente.</p>
+</body>
+</html>
